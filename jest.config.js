8560491dd9d70f4134bc905c80802abb8bea1842
Mario Contreras <mariocc29@gmail.com>
2024-08-11 14:19:08 -0400

    find a solution

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,12 @@
 const jestConfig = {
   transform: {
-    // "\\.(scss|svg)$": "jest-transform-stub",
+    "\\.(scss|svg)$": "jest-transform-stub",
     "^.+\\.jsx?$": "babel-jest",
   },
   setupFilesAfterEnv: ["@testing-library/react", "@testing-library/jest-dom"],
   testEnvironment: "jsdom",
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
   roots: ["<rootDir>/src/tests"],
 };
